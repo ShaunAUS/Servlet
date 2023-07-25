0d7be9361696497611b443928e339f2552ca2082
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2023-07-25 23:17:16 +0900

    controller 헤더추가 /  헤더 조회
    controller 헤더추가 /  헤더 조회

---
+++ mvcTest/src/main/java/com/example/mvcTest/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.example.mvcTest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class TestController {
+
+
+    /**
+     * 특정헤더 조건 매핑
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+
+    /**
+     * HTTP 요청 Content-Type, consume
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * HTTP 요청 Accept, produce
+     */
+
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+
+
+
+}
